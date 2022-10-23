695a4c676382b7c8e2e232e0b4c13c3ced05f48e
Muhammad Yunus <muhammad.yunus@edi-indonesia.co.id>
2022-10-23 12:04:53 +0800

    add tailwind

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,21 @@
 import React from "react";
 import { Logo } from "../../assets/img";
-import "./index.css";
 
 export default function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={Logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-indigo-700 h-screen flex justify-center flex-col items-center">
+      <img src={Logo} className="w-1/2 animate-spin" alt="logo" />
+      <p className="text-3xl text-white font-mono">
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="text-2xl mt-4 text-white underline font-mono"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
