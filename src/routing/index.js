c1fbbb5e932c96b5f080a13d2c629d4ebbf148c1
Muhammad Yunus <muhammad.yunus@edi-indonesia.co.id>
2022-10-23 11:43:15 +0800

    first commit

---
+++ src/routing/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "../components/Layout";
+import { Home } from "../pages";
+
+function Routing() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Routing;
